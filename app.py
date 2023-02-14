41a67d5fd16ffbbbd79c87bc62f7ef82bcb21400
pradeeppathikayala <pradeeppathikayala@outlook.com>
2023-02-14 18:39:59 +0530

    completed web application v1

---
+++ app.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import pickle 
+import os
+from flask import Flask, request, app, jsonify, redirect, render_template
+
+
+
+app = Flask(__name__)
+
+# Loading the model
+reg_model = pickle.load(open('regmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.get_json['data']
+
+
+    print(data)
+    print(np.array(list(data.values())).reshape((1,-1)))
+    x = np.array(list(data.values())).reshape((1,-1))
+    data_std = scaler.transform(x)
+    output = reg_model.predict(data_std)
+
+    return output
+
+if __name__ == '__main__':
+    app.run()
+
